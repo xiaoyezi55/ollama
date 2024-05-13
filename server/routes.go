4a74b4dd0e739b82e9573ba073dbdb2b38bc2bb2
Michael Yang <mxyng@pm.me>
2024-05-13 12:44:58 -0700

    rebase main

--- server/routes.go
+++ server/routes.go
@@ -1041,14 +1041,6 @@ func Serve(ln net.Listener) error {
 
 	slog.SetDefault(slog.New(handler))
 
-	blobsDir, err := GetBlobsPath("")
-	if err != nil {
-		return err
-	}
-	if err := fixBlobs(blobsDir); err != nil {
-		return err
-	}
-
 	if !envconfig.NoPrune {
 		// clean up unused layers and manifests
 		if err := PruneLayers(); err != nil {
@@ -1065,12 +1057,23 @@ func Serve(ln net.Listener) error {
 		}
 	}
 
+	// MIGRATION
+	blobsDir, err := GetBlobsPath("")
+	if err != nil {
+		return err
+	}
+
+	if err := fixBlobs(blobsDir); err != nil {
+		return err
+	}
+
 	// migrate registry.ollama.ai to ollama.com
 	if err := migrateRegistryDomain(); err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
 			return err
 		}
 	}
+	// END MIGRATION
 
 	ctx, done := context.WithCancel(context.Background())
 	schedCtx, schedDone := context.WithCancel(ctx)
