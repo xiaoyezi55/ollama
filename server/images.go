a21de196ce43c613dd0243814ec21325f8d3a7de
jmorganca <jmorganca@gmail.com>
2024-05-18 22:22:46 -0700

    replace static build in `llm`

--- server/images.go
+++ server/images.go
@@ -26,6 +26,7 @@ import (
 	"github.com/ollama/ollama/api"
 	"github.com/ollama/ollama/auth"
 	"github.com/ollama/ollama/format"
+	"github.com/ollama/ollama/llama"
 	"github.com/ollama/ollama/llm"
 	"github.com/ollama/ollama/parser"
 	"github.com/ollama/ollama/envconfig"
@@ -411,7 +412,7 @@ func CreateModel(ctx context.Context, name, modelFileDir, quantization string, m
 						defer temp.Close()
 						defer os.Remove(temp.Name())
 
-						if err := llm.Quantize(blob, temp.Name(), want); err != nil {
+						if err := llama.Quantize(blob, temp.Name(), want); err != nil {
 							return err
 						}
 
